fb42d18c8f86766c8cf24c1756eebe53cac73e09
Harsh Singh <harsh.singh@pointclickcare.com>
2016-08-10 21:49:02 -0400

    Initial Commit

---
+++ InsertingDynamicRows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InsertingDynamicRows
+//
+//  Created by Harsh Singh on 2016-08-10.
+//  Copyright © 2016 Harsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
